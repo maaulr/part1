46304ec04005b8a4492353013da9f6dcf3402036
Maulana Rahman <maulana.auliar@gmail.com>
2020-06-10 12:43:19 +0700

    start building anecdotes

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
 // import * as serviceWorker from './serviceWorker';
+
+const anecdotes_list = [
+  {'text':'If it hurts, do it more often', 'points':0},
+  {'text': 'Adding manpower to a late software project makes it later!', 'points':0},
+  {'text': 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', 'points':0},
+  {'text': 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'points':0},
+  {'text': 'Premature optimization is the root of all evil.', 'points':0},
+  {'text': 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', 'points':0}
+]
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [anecdotes, setAnecdotes] = useState(props.anecdotes_list)
+  console.log(anecdotes)
+  console.log(anecdotes[selected].text)
+
+  return (
+    <div>
+      <h1>anecdotes</h1>
+      <p>{anecdotes[selected].text}</p>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App anecdotes_list={anecdotes_list}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
