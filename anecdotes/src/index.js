a0106b315bcc0803f5e682602f21c133b2425286
Maulana Rahman <maulana.auliar@gmail.com>
2020-06-10 12:57:13 +0700

    change anecdotes list to object

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 // import * as serviceWorker from './serviceWorker';
 
-const anecdotes_list = [
-  {'text':'If it hurts, do it more often', 'points':0},
-  {'text': 'Adding manpower to a late software project makes it later!', 'points':0},
-  {'text': 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', 'points':0},
-  {'text': 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'points':0},
-  {'text': 'Premature optimization is the root of all evil.', 'points':0},
-  {'text': 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', 'points':0}
-]
+const anecdotes_obj = {
+  0:{'text':'If it hurts, do it more often', 'points':0},
+  1:{'text': 'Adding manpower to a late software project makes it later!', 'points':0},
+  2:{'text': 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', 'points':0},
+  3:{'text': 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'points':0},
+  4:{'text': 'Premature optimization is the root of all evil.', 'points':0},
+  5:{'text': 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', 'points':0}
+}
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  const [anecdotes, setAnecdotes] = useState(props.anecdotes_list)
+  const [anecdotes, setAnecdotes] = useState(props.anecdotes_obj)
   console.log(anecdotes)
   console.log(anecdotes[selected].text)
 
@@ -27,7 +27,7 @@ const App = (props) => {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App anecdotes_list={anecdotes_list}/>
+    <App anecdotes_obj={anecdotes_obj}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
